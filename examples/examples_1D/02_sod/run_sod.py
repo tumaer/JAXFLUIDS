9fdc06e7e35946070a6a6793ff8e5283cdfb2099
dbezgin <deniz.bezgin@tum.de>
2024-07-02 18:20:46 -0700

    adding some 1D examples

---
+++ examples/examples_1D/02_sod/run_sod.py
@@ -0,0 +1,44 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+from jaxfluids import InputManager, InitializationManager, SimulationManager
+from jaxfluids_postprocess import load_data, create_1D_figure, create_1D_animation
+
+# SETUP SIMULATION
+input_manager = InputManager("sod.json", "numerical_setup.json")
+initialization_manager = InitializationManager(input_manager)
+sim_manager = SimulationManager(input_manager)
+
+# RUN SIMULATION
+simulation_buffers, time_control_variables, \
+forcing_parameters = initialization_manager.initialization()
+sim_manager.simulate(simulation_buffers, time_control_variables)
+
+# LOAD DATA
+path = sim_manager.output_writer.save_path_domain
+quantities = ["density", "velocity", "pressure"]
+cell_centers, cell_sizes, times, data_dict = load_data(path, quantities)
+
+# PLOT
+plot_dict = {
+    "density": data_dict["density"], 
+    "velocityX": data_dict["velocity"][:,0],
+    "pressure": data_dict["pressure"]
+}
+nrows_ncols = (1,3)
+
+# CREATE ANIMATION
+create_1D_animation(
+    plot_dict,
+    cell_centers,
+    times,
+    nrows_ncols=nrows_ncols,
+    interval=100)
+
+# CREATE FIGURE
+create_1D_figure(
+    plot_dict,
+    cell_centers=cell_centers,
+    nrows_ncols=nrows_ncols,
+    axis="x", axis_values=(0,0), 
+    save_fig="sod_shocktube.png")
