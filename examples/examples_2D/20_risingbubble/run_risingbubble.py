7a740b830fa120b4213d208f35362081e47ef37e
dbezgin <deniz.bezgin@tum.de>
2024-07-10 18:14:14 -0700

    fixed bug in rising bubble

--- examples/examples_2D/20_risingbubble/run_risingbubble.py
+++ examples/examples_2D/20_risingbubble/run_risingbubble.py
@@ -13,9 +13,9 @@ initialization_manager = InitializationManager(input_manager)
 sim_manager = SimulationManager(input_manager)
 
 # RUN SIMULATION
-simulation_buffers, time_control_variables, step_information, \
+simulation_buffers, time_control_variables, \
 forcing_parameters = initialization_manager.initialization()
-sim_manager.simulate(simulation_buffers, time_control_variables, step_information)
+sim_manager.simulate(simulation_buffers, time_control_variables)
 
 # LOAD DATA
 path = sim_manager.output_writer.save_path_domain
